674d63647edba3f33da7e35c4f500303867cea67
Alexis <corporal461@gmail.com>
2023-02-12 01:10:44 +0800

    fix(lualine): wrong colors

--- lua/lualine/themes/nightly.lua
+++ lua/lualine/themes/nightly.lua
@@ -1,32 +1,32 @@
 local nightly = {
   normal = {
-    a = { bg = "NONE", fg = "#BBC2CF" },
+    a = { bg = "NONE", fg = "#7AA2F7" },
     b = { bg = "NONE", fg = "#BBC2CF" },
     c = { bg = "NONE", fg = "#BBC2CF" },
   },
 
   insert = {
-    a = { bg = "NONE", fg = "#BBC2CF" },
+    a = { bg = "NONE", fg = "#98BE65" },
     b = { bg = "NONE", fg = "#BBC2CF" },
   },
 
   command = {
-    a = { bg = "NONE", fg = "#BBC2CF" },
+    a = { bg = "NONE", fg = "#ECBE7B" },
     b = { bg = "NONE", fg = "#BBC2CF" },
   },
 
   visual = {
-    a = { bg = "NONE", fg = "#BBC2CF" },
+    a = { bg = "NONE", fg = "#C678DD" },
     b = { bg = "NONE", fg = "#BBC2CF" },
   },
 
   replace = {
-    a = { bg = "NONE", fg = "#BBC2CF" },
+    a = { bg = "NONE", fg = "#DB4B4B" },
     b = { bg = "NONE", fg = "#BBC2CF" },
   },
 
   terminal = {
-    a = { bg = "NONE", fg = "#BBC2CF" },
+    a = { bg = "NONE", fg = "#7AA2F7" },
     b = { bg = "NONE", fg = "#BBC2CF" },
   },
 
