f6a756136920d210fa98280b36bb417fd743e8a4
Alexis <corporal461@gmail.com>
2023-02-12 00:16:05 +0800

    fix wrong color for lightline

--- lua/lightline/colorscheme/nightly.lua
+++ lua/lightline/colorscheme/nightly.lua
@@ -1,10 +1,10 @@
 local nightly = {
   normal = {
-    left = { { "NONE", "#7AA2F7" }, { "#BBC2CF", "NONE" } },
-    right = { { "NONE", "#7AA2F7" }, { "#BBC2CF", "NONE" } },
-    middle = { { "NONE", "#BBC2CF" } },
-    error = { { "NONE", "#DB4B4B" } },
-    warning = { { "NONE", "#D19A66" } },
+    left = { { "#7AA2F7", "NONE" }, { "#BBC2CF", "NONE" } },
+    right = { { "#7AA2F7", "NONE" }, { "#BBC2CF", "NONE" } },
+    middle = { { "#BBC2CF", "NONE" } },
+    error = { { "#DB4B4B", "NONE" } },
+    warning = { { "#D19A66", "NONE" } },
   },
   inactive = {
     right = { { "NONE", "NONE" }, { "NONE", "NONE" } },
@@ -12,25 +12,30 @@ local nightly = {
     middle = { { "NONE", "NONE" } },
   },
   insert = {
-    left = { { "NONE", "#98BE65" }, { "#BBC2CF", "NONE" } },
-    right = { { "NONE", "#98BE65" }, { "#BBC2CF", "NONE" } },
-    middle = { { "NONE", "#BBC2CF" } },
+    left = { { "#98BE65", "NONE" }, { "#BBC2CF", "NONE" } },
+    right = { { "#98BE65", "NONE" }, { "#BBC2CF", "NONE" } },
+    middle = { { "#BBC2CF", "NONE" } },
   },
   replace = {
-    left = { { "NONE", "#DB4B4B" }, { "#BBC2CF", "NONE" } },
-    right = { { "NONE", "#DB4B4B" }, { "#BBC2CF", "NONE" } },
-    middle = { { "NONE", "#BBC2CF" } },
+    left = { { "#DB4B4B", "NONE" }, { "#BBC2CF", "NONE" } },
+    right = { { "#DB4B4B", "NONE" }, { "#BBC2CF", "NONE" } },
+    middle = { { "#BBC2CF", "NONE" } },
   },
   visual = {
-    left = { { "NONE", "#C678DD" }, { "#BBC2CF", "NONE" } },
-    right = { { "NONE", "#C678DD" }, { "#BBC2CF", "NONE" } },
-    middle = { { "NONE", "#BBC2CF" } },
+    left = { { "#C678DD", "NONE" }, { "#BBC2CF", "NONE" } },
+    right = { { "#C678DD", "NONE" }, { "#BBC2CF", "NONE" } },
+    middle = { { "#BBC2CF", "NONE" } },
+  },
+  command = {
+    left = { { "#ECBE7B", "NONE" }, { "#BBC2CF", "NONE" } },
+    right = { { "#ECBE7B", "NONE" }, { "#BBC2CF", "NONE" } },
+    middle = { { "#BBC2CF", "NONE" } },
   },
   tabline = {
     left = { { "#BBC2CF", "NONE" } },
+    right = { { "#BBC2CF", "NONE" } },
     tabsel = { { "#BBC2CF", "NONE" } },
     middle = { { "#BBC2CF", "NONE" } },
-    right = { { "#BBC2CF", "NONE" } },
   },
 }
 
