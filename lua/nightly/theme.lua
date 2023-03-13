c3f09775a7c1d908e976fc81cb3db705b41b6bcb
Alexis <corporal461@gmail.com>
2023-03-14 05:40:52 +0800

    feat(semantic): don't use semantic highlighting

--- lua/nightly/theme.lua
+++ lua/nightly/theme.lua
@@ -139,16 +139,16 @@ function theme.setup()
     MatchParen = { bold = false, fg = color.yellow },
 
     -- Lsp semantic tokens
-    ["@lsp.type.enum"] = { link = "@type" },
-    ["@lsp.type.keyword"] = { link = "@keyword" },
-    ["@lsp.type.interface"] = { link = "Identifier" },
-    ["@lsp.type.namespace"] = { link = "@namespace" },
-    ["@lsp.type.parameter"] = { link = "@parameter" },
-    ["@lsp.type.property"] = { link = "@property" },
-    ["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
-    ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
-    ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
-    ["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
+    -- ["@lsp.type.enum"] = { link = "@type" },
+    -- ["@lsp.type.keyword"] = { link = "@keyword" },
+    -- ["@lsp.type.interface"] = { link = "Identifier" },
+    -- ["@lsp.type.namespace"] = { link = "@namespace" },
+    -- ["@lsp.type.parameter"] = { link = "@parameter" },
+    -- ["@lsp.type.property"] = { link = "@property" },
+    -- ["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
+    -- ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
+    -- ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
+    -- ["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
 
     -- Lazy
     LazyHandlerTask = { fg = color.green },
